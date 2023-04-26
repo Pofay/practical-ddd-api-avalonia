cc55f77b0d0d4dcc51dad6e38d0461fe4655d603
Pofay <gianCarlo.gilos@gmail.com>
2023-04-26 07:03:33 +0000

    setup: created Money class for use in SnackMachine operations

---
+++ SnackMachine.Logic/Money.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnackMachine.Logic
+{
+    public class Money
+    {
+        public int OneCentCount { get; }
+        public int QuarterCount { get; }
+        public int OneDollarCount { get; }
+        public int FiveDollarCount { get; }
+        public int TwentyDollarCount { get; }
+        public int TenCentCount { get; }
+
+        public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = quarterCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+            );
+
+            return sum;
+        }
+    }
+}
