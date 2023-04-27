c42d6b09c7650d091d763aee60940f8bf37b3d44
Pofay <gianCarlo.gilos@gmail.com>
2023-04-27 07:55:51 +0000

    code-change: turned Money into a record instead to reduce boilerplate

--- SnackMachine.Logic/Money.cs
+++ SnackMachine.Logic/Money.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace SnackMachine.Logic
 {
-    public class Money : ValueObject<Money>
+    public record Money
     {
         public int OneCentCount { get; }
         public int QuarterCount { get; }
@@ -43,28 +43,5 @@ namespace SnackMachine.Logic
             return sum;
         }
 
-        protected override bool EqualsCore(Money other)
-        {
-            return OneCentCount == other.OneCentCount
-                && TenCentCount == other.TenCentCount
-                && QuarterCount == other.QuarterCount
-                && OneDollarCount == other.OneDollarCount
-                && FiveDollarCount == other.FiveDollarCount
-                && TwentyDollarCount == other.TwentyDollarCount;
-        }
-
-        protected override int GetHashCodeCore()
-        {
-            unchecked
-            {
-                int hashCode = OneCentCount;
-                hashCode = (hashCode * 397) ^ TenCentCount;
-                hashCode = (hashCode * 397) ^ QuarterCount;
-                hashCode = (hashCode * 397) ^ OneDollarCount;
-                hashCode = (hashCode * 397) ^ FiveDollarCount;
-                hashCode = (hashCode * 397) ^ TwentyDollarCount;
-                return hashCode;
-            }
-        }
     }
 }
