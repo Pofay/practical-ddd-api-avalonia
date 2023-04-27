2ee8a37fe0174ee3f2952689ad4f9f0d38ebec70
Pofay <gianCarlo.gilos@gmail.com>
2023-04-27 08:43:37 +0000

    feat: Can now properly calculate amount and have MoneyInside once buying a snack inside the SnackMachine.
    note: Renamed SnackMachine -> SnackMachineEntity due to namespace collisions

---
+++ SnackMachine.Logic/SnackMachineEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace SnackMachine.Logic;
+
+public sealed class SnackMachineEntity : Entity
+{
+    private static readonly Money[] ACCEPTABLE_COINS_AND_NOTES = { Money.Cent, Money.TenCent, Money.Quarter, Money.Dollar, Money.FiveDollar, Money.TwentyDollar };
+    public Money MoneyInside { get; private set; } = Money.None;
+    public Money MoneyInTransaction { get; private set; } = Money.None;
+
+    public void InsertMoney(Money money)
+    {
+        if (!ACCEPTABLE_COINS_AND_NOTES.Contains(money))
+            throw new InvalidOperationException();
+
+        MoneyInTransaction += money;
+    }
+
+    public void ReturnMoney()
+    {
+        MoneyInTransaction = Money.None;
+    }
+
+    public void BuySnack()
+    {
+        MoneyInside += MoneyInTransaction;
+        MoneyInTransaction = Money.None;
+    }
+}
---
+++ SnackMachine.Tests/SnackMachineEntityTest.cs
@@ -0,0 +1,66 @@
+using Xunit;
+using SnackMachine.Logic;
+using FluentAssertions;
+
+
+namespace SnackMachine.Tests
+{
+    public class SnackMachineEntityTest
+    {
+
+        [Fact]
+        public void ShouldNotContainAnyMoneyInsideOrMoneyInTransactionOnCreation()
+        {
+            var sut = new SnackMachineEntity();
+
+            sut.MoneyInside.Should().Be(Money.None);
+            sut.MoneyInTransaction.Should().Be(Money.None);
+        }
+
+        [Fact]
+        public void ShouldEmptyMoneyInTransactionWhenInvokingReturnMoney()
+        {
+            var sut = new SnackMachineEntity();
+            sut.InsertMoney(Money.Dollar);
+
+            sut.ReturnMoney();
+
+            sut.MoneyInTransaction.Amount.Should().Be(0m);
+        }
+
+        [Fact]
+        public void ShouldInsertMoneyToMoneyInTransaction()
+        {
+            var sut = new SnackMachineEntity();
+
+            sut.InsertMoney(Money.Cent);
+            sut.InsertMoney(Money.Dollar);
+
+            sut.MoneyInTransaction.Amount.Should().Be(1.01m);
+        }
+
+        [Fact]
+        public void ShouldNotBeAbleToInsertMoreThanOneCoinOrNoteAtATime()
+        {
+            var sut = new SnackMachineEntity();
+            var twoCent = Money.Cent + Money.Cent;
+
+            var action = () => sut.InsertMoney(twoCent);
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void ShouldPutMoneyInTranstionToMoneyInsideOnBuySnack()
+        {
+            var sut = new SnackMachineEntity();
+            sut.InsertMoney(Money.Dollar);
+            sut.InsertMoney(Money.Dollar);
+
+            sut.BuySnack();
+
+            sut.MoneyInside.Amount.Should().Be(2m);
+            sut.MoneyInTransaction.Amount.Should().Be(0m);
+        }
+    }
+}
