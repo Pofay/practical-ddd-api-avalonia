848fce62b0de699250107b07cc5cef7dd13952d6
Pofay <gianCarlo.gilos@gmail.com>
2023-05-03 11:32:50 +0800

    setup: Added Required packages and initial class for EntityFramework

---
+++ SnackMachine.Logic/Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SnackMachine.Logic.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<SnackMachineEntity> SnackMachines { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options) { }
+    }
+
+
+}
