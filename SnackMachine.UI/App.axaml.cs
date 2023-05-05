b6fa3bd99b9db971643c3b4906a8646ca9b74aec
Gian Carlo I. Gilos <gianCarlo.gilos@gmail.com>
2023-05-05 16:28:48 +0800

    feat: Setup EntityFramework connection in App initialization and verified database connection works

--- SnackMachine.UI/App.axaml.cs
+++ SnackMachine.UI/App.axaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Shapes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using SnackMachine.Logic;
+using SnackMachine.Logic.Persistence;
 using SnackMachine.UI.ViewModels;
 using SnackMachine.UI.Views;
 
@@ -25,9 +27,23 @@ namespace SnackMachine.UI
                 // Line below is needed to remove Avalonia data validation.
                 // Without this line you will get duplicate validations from both Avalonia and CT
                 ExpressionObserver.DataValidators.RemoveAll(x => x is DataAnnotationsValidationPlugin);
+
+                SnackMachineEntity snackMachine;
+                using (var dbContext = new DataContextFactory().CreateDbContext(new string[] { Environment.GetEnvironmentVariable("DATABASE_URL") }))
+                {
+                    var snackMachineId = Guid.Parse("09213a9c-ff65-4b01-b7da-ac7a792b119e");
+                    var existingSnackMachine = dbContext.SnackMachines.Find(snackMachineId);
+                    if (existingSnackMachine == null)
+                    {
+                        existingSnackMachine = new SnackMachineEntity(snackMachineId);
+                        dbContext.SnackMachines.Add(existingSnackMachine);
+                        dbContext.SaveChanges();
+                    }
+                    snackMachine = existingSnackMachine;
+                }
                 desktop.MainWindow = new SnackMachineWindow
                 {
-                    DataContext = new SnackMachineViewModel(new SnackMachineEntity(Guid.NewGuid())),
+                    DataContext = new SnackMachineViewModel(snackMachine)
                 };
             }
 
