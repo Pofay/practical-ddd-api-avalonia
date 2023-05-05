6e4f13dc3f89f29df4b60dd6b63bd30b0d7b6b4c
Gian Carlo I. Gilos <gianCarlo.gilos@gmail.com>
2023-05-05 16:09:15 +0800

    feat: Application can now successfully save SnackMachineEntity to the database

---
+++ SnackMachine.Tests/EntityFrameworkSanityTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SnackMachine.Logic;
+using SnackMachine.Logic.Persistence;
+
+namespace SnackMachine.Tests
+{
+    public class EntityFrameworkSanityTests
+    {
+
+        [Fact]
+        public async void ShouldReturnSavedSnackMachine()
+        {
+            var id = Guid.NewGuid();
+            var expected = new SnackMachineEntity(id);
+
+            using (var context = new DataContextFactory().CreateDbContext(new string[] { "Server=localhost; Port=5432; User Id=postgres; Password=postgres; Database=practical_ddd_db; CommandTimeout=20;" }))
+            {
+                context.SnackMachines.Add(expected);
+                await context.SaveChangesAsync();
+
+                var actual = await context.SnackMachines.FindAsync(id);
+                Assert.NotNull(actual);
+                Assert.Equal(actual, expected);
+
+                context.SnackMachines.Remove(actual);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
