54bce2c9452d5308797fc409e6de4632463544a0
Gian Carlo I. Gilos <gianCarlo.gilos@gmail.com>
2023-05-11 20:40:29 +0800

    code-change: Created SnackPile as Value Object to resolve awkward method calls for determining quantity of a snack when buying a snack

---
+++ SnackMachine.Logic/SnackPile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackMachine.Logic
+{
+    public record SnackPile
+    {
+        public Snack Snack { get; }
+        public int Quantity { get; }
+        public decimal Price { get; }
+
+        private SnackPile() { }
+
+        public SnackPile(Snack snack, int quantity, decimal price) : this()
+        {
+            if (quantity < 0)
+                throw new InvalidOperationException();
+            if(price < 0)
+                throw new InvalidOperationException();
+            if (price % 0.01m > 0)
+                throw new InvalidOperationException();
+            Snack = snack;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public SnackPile SubtractOne()
+        {
+            return new SnackPile(Snack, Quantity - 1, Price);
+        }
+    }
+}
---
+++ SnackMachine.Tests/SnackPileTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnackMachine.Logic;
+using Xunit;
+
+namespace SnackMachine.Tests
+{
+    public class SnackPileTest
+    {
+        [Fact]
+        public void ShouldThrowExceptionWhenCreatingWithQuantityLessThan0()
+        {
+            Assert.Throws<InvalidOperationException>(() => { new SnackPile(null, -1, 1m); });
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenCreatingWithPriceLessThan0()
+        {
+            Assert.Throws<InvalidOperationException>(() => { new SnackPile(null, 1, -1); });
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenCreatingWithAPriceMorePreciseThanOneCent()
+        {
+            Assert.Throws<InvalidOperationException>(() => { new SnackPile(null, 1, 0.001m); });
+        }
+    }
+}
