21cd33be5390267d2da85932eb39723c3415718c
Gian Carlo I. Gilos <gianCarlo.gilos@gmail.com>
2023-05-19 14:37:33 +0800

    migrations: Changed setting for Slot -> SnackMachine to enable navigability

--- SnackMachine.Logic/Migrations/20230519040421_AdditionalNavigationForSlotToSnackMachine.Designer.cs
+++ SnackMachine.Logic/Migrations/20230519040421_AdditionalNavigationForSlotToSnackMachine.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SnackMachine.Logic.Persistence;
@@ -11,9 +12,11 @@ using SnackMachine.Logic.Persistence;
 namespace SnackMachine.Logic.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230519040421_AdditionalNavigationForSlotToSnackMachine")]
+    partial class AdditionalNavigationForSlotToSnackMachine
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
