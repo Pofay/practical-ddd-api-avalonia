f115729d9a120fdefc7a87f0b59f96633227f3ab
Gian Carlo I. Gilos <gianCarlo.gilos@gmail.com>
2023-05-22 17:13:16 +0800

    migrations: Change generation strategy for SnackId to None since most instances are static

--- SnackMachine.Logic/Persistence/Configuration/SnackEntityTypeConfiguration.cs
+++ SnackMachine.Logic/Persistence/Configuration/SnackEntityTypeConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace SnackMachine.Logic.Persistence.Configuration
         public void Configure(EntityTypeBuilder<Snack> builder)
         {
             builder.ToTable(nameof(Snack));
-            builder.HasKey(x => x.Id);
+            builder.HasKey(x => x.Id).HasAnnotation("DatabaseGenerated", DatabaseGeneratedOption.None);
             builder.Property(x => x.Name);
         }
     }
